9e0668e6c2ebd4f968c79a457b93120c24664609
Arjen Zwep <arjen.zwep@6gorillas.nl>
2022-10-12 17:27:16 +0200

    fixed docker setup

--- api/config.py
+++ api/config.py
@@ -4,7 +4,7 @@ from urllib import parse
 basedir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'Sqlite/skinapp.db'))
 
 # params = parse.quote_plus(os.environ["DB_CONN_STRING"])
-db_uri = f"postgresql://test:Test123!@localhost:5432/skintable"
+db_uri = f"postgresql://test:Test123!@host.docker.internal:5432/skintable"
 #db_uri = f"sqlite:///{basedir}"
 
 class Config:
